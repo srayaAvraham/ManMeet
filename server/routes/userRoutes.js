5e468e144b101705e36df6e1fe4f18a981045865
Baraks24 <steindl.barak@gmail.com>
2018-03-25 12:55:21 +0300

    add user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,21 +5,13 @@ const express = require('express')
 const router = express.Router()
 const user_controller = require('../controllers/userController');
 
-router.get('/users',user_controller.list_all_users)
+router.route('/users')
+      .get(user_controller.list_all_users)
+      .post(user_controller.create_a_user)
+
+router.route('/users/:userId')
+      .get(user_controller.read_a_user)
+      .put(user_controller.update_a_user)
+      .delete(user_controller.delete_a_user)
 
 module.exports = router;
-
-// module.exports = function(app) {
-//   var user = require('../controllers/userController');
-
-//   // todoList Routes
-//   app.route('/users')
-//     .get(user.list_all_users)
-//     .post(user.create_a_user);
-
-
-//   app.route('/users/:userId')
-//     .get(user.read_a_user)
-//     .put(user.update_a_user)
-//     .delete(user.delete_a_user);
-// };
